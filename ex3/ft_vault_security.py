aeda15e8ac8754de39e1ba7c9d3e90bca74adf32
Adrien Vacher <advacher@z2r9p3.42lyon.fr>
2026-02-06 15:05:40 +0100

    manage try/except ex3

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -9,18 +9,26 @@ It ensures secure handling of sensitive information.
 def main() -> None:
     print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
     print("Initiating secure vault access...")
-    with open("classified_data.txt", "r") as file:
-        print("Vault connection established with failsafe protocols\n")
-        print("SECURE EXTRACTION:")
-        print(file.read())
-        print()
-    with open("security_protocols.txt", "w") as file_w:
-        print("SECURE PRESERVATION:")
-        entry_1 = "[CLASSIFIED] New security protocols archived\n"
-        file_w.write(entry_1)
-        print(f"{entry_1.strip()}")
-        print("Vault automatically sealed upon completion\n")
-    print("All vault operations completed with maximum security.")
+    try:
+        with open("classified_data.txt", "r") as file:
+            print("Vault connection established with failsafe protocols\n")
+            print("SECURE EXTRACTION:")
+            print(file.read())
+            print()
+        with open("security_protocols.txt", "w") as file_w:
+            print("SECURE PRESERVATION:")
+            entry_1 = "[CLASSIFIED] New security protocols archived\n"
+            file_w.write(entry_1)
+            print(f"{entry_1.strip()}")
+            print("Vault automatically sealed upon completion\n")
+    except PermissionError as e:
+        print(f"ERROR: {e}.")
+    except FileNotFoundError as e:
+        print(f"ERROR: {e} not found.")
+    except Exception as e:
+        print(f"RESPONSE: Unexpected anomaly: {e}")
+    finally:
+        print("All vault operations completed with maximum security.")
 
 
 if __name__ == "__main__":
